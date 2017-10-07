6bc980720b1db1658bcaa8ce351031dcd05922ca
jdrumelhart <jdrumelhart@gmail.com>
2017-10-07 15:55:18 -0700

    Trouble with structure

--- src/Robot.java
+++
@@ -1,5 +0,0 @@
-import edu.wpi.first.wpilibj.IterativeRobot;
-
-public class Robot extends IterativeRobot {
-	
-}
---
+++ src/main/Robot.java
@@ -0,0 +1,44 @@
+package main;
+
+import java.util.ArrayList;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Robot extends IterativeRobot{
+	
+	@Override
+	public void robotInit() {
+		
+	}	
+	
+	@Override
+	public void autonomousInit() {
+		
+	}	
+	
+	@Override
+	public void autonomousPeriodic() {
+		
+	}	
+	
+	@Override
+	public void teleopInit() {
+		
+	}	
+	
+	@Override
+	public void teleopPeriodic() {
+		
+	}
+	
+	@Override
+	public void disabledInit() {
+		
+	}
+	
+	@Override
+	public void disabledPeriodic() {
+		
+	}
+	
+}
