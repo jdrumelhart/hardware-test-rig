69a9cbe9273c41ac0e11a1c7accc89ce38223ee4
jdrumelhart <jdrumelhart@gmail.com>
2017-10-07 16:39:40 -0700

    Began creating hardware parts

--- src/main/Robot.java
+++ src/main/Robot.java
@@ -2,13 +2,19 @@ package main;
 
 import java.util.ArrayList;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.TalonSRX;
 
 public class Robot extends IterativeRobot{
 	
 	@Override
 	public void robotInit() {
-		
+		Encoder e1 = new Encoder(0, 1, false);
+		Encoder e2 = new Encoder(2, 3, true);
+		TalonSRX t1 = new TalonSRX(4);
+		TalonSRX t2 = new TalonSRX(5);
+
 	}	
 	
 	@Override
