190f092cbf454f368736afe1aad1d9407ce49d92
jdrumelhart <jdrumelhart@gmail.com>
2017-11-18 14:44:55 -0800

    Created InputObject to handle hardware creation

---
+++ src/main/InputObject.java
@@ -0,0 +1,11 @@
+package main;
+
+public class InputObject implements Comparable<InputObject>{
+
+	@Override
+	public int compareTo(InputObject o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
--- src/main/ModularHardwareAdapter.java
+++ src/main/ModularHardwareAdapter.java
@@ -2,9 +2,8 @@ package main;
 
 public class ModularHardwareAdapter {
 	
-	private static final ModularHardwareAdapter instance = new ModularHardwareAdapter();
-
-	public static ModularHardwareAdapter getInstance() {
-		return instance;
+	public ModularHardwareAdapter(InputObject[] a) {
+		
 	}
+	
 }
