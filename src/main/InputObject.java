741cffe399464746cc4ecd8704bf1ceb7c406ca0
jdrumelhart <jdrumelhart@gmail.com>
2017-11-18 16:06:38 -0800

    Implemented basic InputObject functionality

--- src/main/InputObject.java
+++ src/main/InputObject.java
@@ -1,11 +1,32 @@
 package main;
 
-public class InputObject implements Comparable<InputObject>{
+import java.util.ArrayList;
 
+public class InputObject implements Comparable<InputObject>{
+	private Object classType = null;
+	private ArrayList<?> fields = null;
+	
+	public InputObject(Object type, ArrayList<?> in) {
+		classType = type;
+		fields = in;
+	}
+	
+	public Object getClassType() {
+		return classType;
+	}
+	
+	public ArrayList<?> getFields() {
+		return fields;
+	}
+	
 	@Override
 	public int compareTo(InputObject o) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 	
+	@Override
+	public String toString() {
+		return classType.toString() + "/n" + fields.toString();
+	}
 }
