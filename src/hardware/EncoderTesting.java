702a35c9737479e60aae22fec2228d8c04d8dedf
jdrumelhart <jdrumelhart@gmail.com>
2017-12-09 15:56:10 -0800

    General cleanup

--- src/hardware/EncoderTesting.java
+++ src/hardware/EncoderTesting.java
@@ -6,12 +6,10 @@ public class EncoderTesting extends Encoder implements HardwareInterface {
 
 	public EncoderTesting(int channelA, int channelB) {
 		super(channelA, channelB);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void selfTest() {
-		// TODO Auto-generated method stub
 		
 	}
 	
--- src/main/ModularHardwareAdapter.java
+++ src/main/ModularHardwareAdapter.java
@@ -1,24 +1,14 @@
 package main;
 
 import edu.wpi.first.wpilibj.Encoder;
+import hardware.HardwareInterface;
 
 public class ModularHardwareAdapter {
 	
-	public ModularHardwareAdapter(Class<?>... input) {
-		for(Class<?> i: input) {
-			createHardware(i);
+	public ModularHardwareAdapter(HardwareInterface... input) {
+		for(HardwareInterface i: input) {
+			i.selfTest();
 		}
 	}
-	
-	private void createHardware(Class<?> input) {
-		if(input.isInstance(new Encoder(0, 0))) {
-			try {
-				Class<?> cls = Class.forName("Encoder");
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
 		
-	}
 }
